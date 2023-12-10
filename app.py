f2579c6e4e2f33995c697cd49a44a35145c7220e
Vishal Lavangare <vishal.lavangare@outlook.com>
2023-12-10 13:06:54 +0530

    minor app update

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if st.button('Predict'):
     # 3. predict
     result = model.predict(vector_input)[0]
     # 4. Display
-    if result == 0:
+    if result == 1:
         st.header("Spam")
     else:
         st.header("Not Spam")
