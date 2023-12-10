19a453e4b569abd70821f0341e91c8e411e4e1f2
Vishal Lavangare <vishal.lavangare@outlook.com>
2023-12-10 13:19:56 +0530

    update

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if st.button('Predict'):
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
     # 3. predict
-    result = model.predict(vector_input)[0]
+    result = model.predict(vector_input)[1]
     # 4. Display
     if result == 1:
         st.header("Spam")
